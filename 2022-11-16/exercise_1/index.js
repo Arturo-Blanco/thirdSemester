7a578989e54ee1e7cd2ee132e22e6f97bcadb8c7
Arturo Blanco <blancoarturo92@gmail.com>
2022-11-20 14:36:12 -0300

    edit files

--- 2022-11-16/exercise_1/index.js
+++ 2022-11-16/exercise_1/index.js
@@ -1,38 +1,40 @@
 "use strict";
-
+let title=document.querySelector(".title");
+let url=document.querySelector(".url");
+let textArea=document.querySelector(".textArea");
 let btnCrear=document.getElementById("btnCrear");
 btnCrear.addEventListener('click',() => {
     loadTitle();
     loadImage();
     loadBody();
+    console.log("Hola");
 });
-
 function loadTitle(){
-    let title=document.querySelector(".title");
-    let aux=title.value
-    if(aux!==" " && aux!==undefined){
+    
+    let aux=title.value;
+    if(aux!=="" && aux!==undefined){
         document.querySelector(".card-title").innerHTML=aux;
-        document.querySelector(".title").value=" ";
+        document.querySelector(".title").value="";
     } else {
         alert("Ingrese un titulo para su tarjeta");
     }
 }
 function loadImage(){
-    let url=document.querySelector(".url");
+    
     let aux=url.value;
-    if(aux!==" " && aux!==undefined){
+    if(aux!=="" && aux!==undefined){
         document.querySelector(".card-img-top").src=aux;
-        document.querySelector(".url").value=" ";
+        document.querySelector(".url").value="";
     } else {
         alert("Ingrese una direccion de imagen valida");
     }
 }
 function loadBody(){
-    let textArea=document.querySelector(".textArea");
+    
     let aux=textArea.value;
-    if(aux!==" " && aux!==undefined){
+    if(aux!=="" && aux!==undefined){
         document.querySelector(".card-text").innerHTML=aux;
-        document.querySelector(".textArea").value=" ";
+        document.querySelector(".textArea").value="";
     } else {
         alert("Ingrese un contenido para su tarjeta");
     }
